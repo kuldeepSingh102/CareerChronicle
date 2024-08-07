cf64706f3811af79a00801fcb90bd4504daca046
Kuldeep Singh <ks2sept2005ks@gmail.com>
2024-08-07 20:45:11 +0530

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+import links from "@/utils/links";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Logo from "../assets/logo.svg";
+
+export default function Sidebar() {
+  const pathname = usePathname();
+  return (
+    <aside className="py-4 px-8 bg-muted h-full">
+      <Image src={Logo} alt="logo" className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map((link) => (
+          <Button
+            asChild
+            key={link.href}
+            variant={pathname === link.href ? "default" : "link"}
+          >
+            <Link href={link.href} className="flex items-center gap-x-2">
+              {link.icon}
+              <span className="capitalize">{link.label}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+    </aside>
+  );
+}
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ThemeToggle() {
+  return (
+    <div>
+      ThemeToggle
+    </div>
+  )
+}
+
+export default ThemeToggle
